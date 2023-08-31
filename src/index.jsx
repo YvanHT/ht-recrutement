df21d7a0e34a403f585cd5cbe10a263241e83b88
YvanHT <yvan@hightao-mg.com>
2023-09-01 01:09:52 +0300

    migrate to current version

--- src/index.jsx
+++ src/index.jsx
@@ -4,11 +4,13 @@ import './index.scss';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
+const rootElement = document.getElementById('root');
+
+const root = ReactDOM.createRoot(rootElement);
+root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root'),
 );
 
 // If you want to start measuring performance in your app, pass a function
